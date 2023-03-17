36d8823092543b890e4b1ca9ed3730f4ecca36b1
parth <purohitparth1@gmail.com>
2023-03-17 12:00:35 +0530

    17-03-2023

---
+++ Class work 17-03-2023.java
@@ -0,0 +1,52 @@
+package Core;
+
+public class Test_2 {
+	public static void main(String[] args) {
+		int a=15, b=30;
+		System.out.println("summation :"+(a+b));
+		System.out.println("substraction :"+(a-b));
+		System.out.println("multiplication :"+(a*b));
+		System.out.println("division :"+((float)a/b));
+		
+		
+		/* Variables: It is a name at memory location, which stores value of different types.
+		*Naming convention for variables.
+		*
+		*1) It should not start with digit
+		*2) No special characters allowed except _
+		*3) No whitespace e.g first name; not allowed
+		*4) It is case sensitive
+		*
+		*e.g   a is different and A is different
+		*
+		*because they follow ascii rule
+		*ascii( American standard code for information interchange)
+		*
+		*Datatypes
+		*There are 2 datatypes
+		*
+		*1) Primitive datatypes
+		*2) Non-primitive data types
+		*
+		*1) Primitive data types
+		*
+		*data types             size         class
+		*-byte                   1 byte       Byte
+		*-short                  1 byte       Short
+		*-char                   1 byte       Character
+		*-int                     4 byte       Integer
+		*-float                   4 byte       Float  
+		*-double                8 byte       Double
+		*-long                   8 byte       Long
+		*boolean               true/false    Boolean
+		*
+		*
+		*2) Non primitive data type or Derived class
+		*
+		*1) String
+		*2) Array
+		*3) Class
+		*/
+	}
+
+}
